0aa085dd473579e73d8ed7b743c4f2cecea0d86b
Ilya Yeuzhanka <evzhenko1106@gmail.com>
2022-11-01 23:12:53 +0300

    added alembic support

---
+++ alembic/versions/1_init_migration.py
@@ -0,0 +1,30 @@
+"""empty message
+
+Revision ID: 8e8779e25503
+Revises: 
+Create Date: 2022-11-01 19:50:55.595656
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8e8779e25503'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table('user', sa.Column('username', sa.String(length=256), nullable=False),
+                    sa.Column('email', sa.String(length=256), nullable=False),
+                    sa.Column('hashed_password', sa.String(length=256), nullable=False))
+    op.create_table('post', sa.Column('title', sa.String(length=256), nullable=False),
+                    sa.Column('content', sa.String(length=256), nullable=False),
+                    sa.Column('published', sa.Boolean, nullable=False, server_default='1'),
+                    sa.Column('created_at', sa.TIMESTAMP, nullable=False))
+
+
+def downgrade() -> None:
+    op.drop_table('tags')
